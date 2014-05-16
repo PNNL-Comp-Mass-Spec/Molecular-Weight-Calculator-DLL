a90e3e6aefef1a73bbe829b91772c355d5b00194
alchemistmatt <matthew.monroe@pnnl.gov>
2014-05-16 10:22:20 -0700

    Remove batch fiels

--- MwtWinDll_SourceCode/SourceAndSupportingDLLs/Update_Supporting_Source_Code.bat
+++
@@ -1,9 +0,0 @@
-@echo on
-rem Del SmoothProgressBar_Source*.zip
-rem Del ProgressForm_Source*.zip
-
-@echo off
-echo;
-
-rem Copy "F:\My Documents\Projects\DataMining\SmoothProgressBarDLL\SmoothProgressBar_SourceCode\*.zip" .
-rem Copy "F:\My Documents\Projects\DataMining\ProgressFormDLL\ProgressForm_SourceCode\*.zip" .
