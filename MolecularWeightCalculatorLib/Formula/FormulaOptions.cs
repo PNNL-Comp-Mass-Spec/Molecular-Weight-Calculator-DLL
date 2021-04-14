26a9abd4d6585cc68f18f12746b67cb33053ef1c
alchemistmatt <matthew.monroe@pnnl.gov>
2021-04-14 12:47:11 -0700

    Add documentation comments

--- MolecularWeightCalculatorLib/Formula/FormulaOptions.cs
+++ MolecularWeightCalculatorLib/Formula/FormulaOptions.cs
@@ -5,12 +5,56 @@ namespace MolecularWeightCalculator.Formula
     [ComVisible(false)]
     public class FormulaOptions
     {
+        // Ignore Spelling: Arial
+
+        /// <summary>
+        /// Abbreviation recognition mode
+        /// </summary>
+        /// <remarks>
+        /// NormalOnly, NormalPlusAminoAcids (default), or NoAbbreviations
+        /// </remarks>
         public AbbrevRecognitionMode AbbrevRecognitionMode { get; set; }
+
+        /// <summary>
+        /// When true, treat square brackets as parentheses
+        /// </summary>
+        /// <remarks>Default is true</remarks>
         public bool BracketsAsParentheses { get; set; }
+
+        /// <summary>
+        /// Case conversion mode
+        /// </summary>
+        /// <remarks>
+        /// ConvertCaseUp (default), ExactCase, or SmartCase
+        /// </remarks>
         public CaseConversionMode CaseConversion { get; set; }
+
+        /// <summary>
+        /// Decimal point symbol
+        /// </summary>
+        /// <remarks>
+        /// Typically a period, but if 5,500 matches 5.5 then a comma
+        /// </remarks>
         public char DecimalSeparator { get; set; }
+
+        /// <summary>
+        /// Rich text font name
+        /// </summary>
+        /// <remarks>Default is Arial</remarks>
         public string RtfFontName { get; set; }
+
+        /// <summary>
+        /// Rich text size
+        /// </summary>
+        /// <remarks>Default is 10</remarks>
         public short RtfFontSize { get; set; }
-        public StdDevMode StdDevMode { get; set; } // Can be 0, 1, or 2 (see StdDevModeConstants)
+
+        /// <summary>
+        /// Standard deviation mode
+        /// </summary>
+        /// <remarks>
+        /// Short, Scientific, or Decimal (default)
+        /// </remarks>
+        public StdDevMode StdDevMode { get; set; }
     }
 }
