f218c0cc5c730c87453e48e130c610a12e5f4183
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2021-04-20 09:52:51 -0700

    Change version to use NerdBank.GitVersioning

--- MolecularWeightCalculatorLib/Properties/AssemblyInfo.cs
+++ MolecularWeightCalculatorLib/Properties/AssemblyInfo.cs
@@ -30,4 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.2.*")]
+
+// SEE version.json to update the version!!!
