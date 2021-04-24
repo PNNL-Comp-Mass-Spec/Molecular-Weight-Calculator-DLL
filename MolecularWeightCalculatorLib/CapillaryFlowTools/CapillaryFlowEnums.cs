66e487bb1f946ec572fcb47c0bbd6c1ddfba18b1
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2021-04-23 21:42:51 -0700

    Add 'Description' attributes to some enums for basic user-friendly display in a GUI

--- MolecularWeightCalculatorLib/CapillaryFlowTools/CapillaryFlowEnums.cs
+++ MolecularWeightCalculatorLib/CapillaryFlowTools/CapillaryFlowEnums.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
 
 // ReSharper disable once CheckNamespace
 namespace MolecularWeightCalculator
@@ -6,141 +7,212 @@ namespace MolecularWeightCalculator
     [Guid("9DACDA73-0875-41D8-AECA-90651C7DC510"), ComVisible(true)]
     public enum CapillaryType
     {
+        [Description("Open Tubular Capillary")]
         OpenTubularCapillary = 0,
+        [Description("Packed Capillary")]
         PackedCapillary
     }
 
     [Guid("ED5D8D28-FE6D-4FE4-B0F6-1F521971A3D1"), ComVisible(true)]
     public enum UnitOfPressure
     {
+        [Description("psi")]
         Psi = 0,
+        [Description("Pascals")]
         Pascals,
+        [Description("kiloPascals")]
         KiloPascals,
+        [Description("Atmospheres")]
         Atmospheres,
+        [Description("Bar")]
         Bar,
+        [Description("Torr (mm Hg)")]
         Torr,
+        [Description("dynes/cm^2")]
         DynesPerSquareCm
     }
 
     [Guid("76D1C162-CAE8-4921-9928-DF229775B629"), ComVisible(true)]
     public enum UnitOfLength
     {
+        [Description("m")]
         M = 0,
+        [Description("cm")]
         CM,
+        [Description("mm")]
         MM,
+        [Description("um")]
         Microns,
+        [Description("inches")]
         Inches
     }
 
     [Guid("FDB804A9-0A8C-469E-86CB-3040A7B5116C"), ComVisible(true)]
     public enum UnitOfViscosity
     {
+        [Description("Poise [g/(cm-sec)]")]
         Poise = 0,
+        [Description("centiPoise")]
         CentiPoise
     }
 
     [Guid("88C1C17B-88A2-4B1F-B739-C78FD27D6FEB"), ComVisible(true)]
     public enum UnitOfFlowRate
     {
+        [Description("mL/min")]
         MLPerMin = 0,
+        [Description("uL/min")]
         ULPerMin,
+        [Description("nL/min")]
         NLPerMin
     }
 
     [Guid("5191EEFB-1AB7-452D-BF6D-D8F6139B53DB"), ComVisible(true)]
     public enum UnitOfLinearVelocity
     {
+        [Description("cm/hr")]
         CmPerHr = 0,
+        [Description("mm/hr")]
         MmPerHr,
+        [Description("cm/min")]
         CmPerMin,
+        [Description("mm/min")]
         MmPerMin,
+        [Description("cm/sec")]
         CmPerSec,
+        [Description("mm/sec")]
         MmPerSec
     }
 
     [Guid("94F70EBF-8E33-42A2-83C0-F98BD138078E"), ComVisible(true)]
     public enum UnitOfTime
     {
+        [Description("hours")]
         Hours = 0,
+        [Description("minutes")]
         Minutes,
+        [Description("seconds")]
         Seconds
     }
 
     [Guid("97E85E7B-3319-4FC3-87E5-60D3B5A28B91"), ComVisible(true)]
     public enum UnitOfVolume
     {
+        [Description("mL")]
         ML = 0,
+        [Description("uL")]
         UL,
+        [Description("nL")]
         NL,
+        [Description("pL")]
         PL
     }
 
     [Guid("1DAEC84C-DD67-4E62-9975-53B74C22BF3C"), ComVisible(true)]
     public enum UnitOfConcentration
     {
+        [Description("Molar")]
         Molar = 0,
+        [Description("milliMolar")]
         MilliMolar,
+        [Description("microMolar")]
         MicroMolar,
+        [Description("nanoMolar")]
         NanoMolar,
+        [Description("picoMolar")]
         PicoMolar,
+        [Description("femtoMolar")]
         FemtoMolar,
+        [Description("attoMolar")]
         AttoMolar,
+        [Description("mg/mL")]
         MgPerML,
+        [Description("ug/mL")]
         UgPerML,
+        [Description("ng/mL")]
         NgPerML,
+        [Description("ug/uL")]
         UgPerUL,
+        [Description("ng/uL")]
         NgPerUL
     }
 
     [Guid("19EFF331-E247-4733-BF5C-F9E87285DF87"), ComVisible(true)]
     public enum UnitOfTemperature
     {
+        [Description("Celsius")]
         Celsius = 0,
+        [Description("Kelvin")]
         Kelvin,
+        [Description("Fahrenheit")]
         Fahrenheit
     }
 
     [Guid("5726243F-E12C-409E-AC8F-1095436C397D"), ComVisible(true)]
     public enum UnitOfMassFlowRate
     {
+        [Description("pmol/min")]
         PmolPerMin = 0,
+        [Description("fmol/min")]
         FmolPerMin,
+        [Description("amol/min")]
         AmolPerMin,
+        [Description("pmol/sec")]
         PmolPerSec,
+        [Description("fmol/sec")]
         FmolPerSec,
+        [Description("amol/sec")]
         AmolPerSec,
+        [Description("mol/min")]
         MolesPerMin
     }
 
     [Guid("0E982894-E072-4122-930D-B5E4583BB31E"), ComVisible(true)]
     public enum UnitOfMolarAmount
     {
+        [Description("Moles")]
         Moles = 0,
+        [Description("milliMoles")]
         MilliMoles,
+        [Description("microMoles")]
         MicroMoles,
+        [Description("nanoMoles")]
         NanoMoles,
+        [Description("picoMoles")]
         PicoMoles,
+        [Description("femtoMoles")]
         FemtoMoles,
+        [Description("attoMoles")]
         AttoMoles
     }
 
     [Guid("D00EA5CC-DC9C-44CE-A96F-649793625D1B"), ComVisible(true)]
     public enum UnitOfDiffusionCoefficient
     {
+        [Description("cm^2/hr")]
         CmSquaredPerHr = 0,
+        [Description("cm^2/min")]
         CmSquaredPerMin,
+        [Description("cm^2/sec")]
         CmSquaredPerSec
     }
 
     [Guid("DE5DBA1F-A19C-4EB2-AFF4-7F3770ECFB5F"), ComVisible(true)]
     public enum AutoComputeMode
     {
+        [Description("Find Back Pressure")]
         BackPressure = 0,
+        [Description("Find Inner Diameter")]
         ColumnId,
+        [Description("Find Column Length")]
         ColumnLength,
+        [Description("Find Dead Time")]
         DeadTime,
+        [Description("Find Linear Velocity")]
         LinearVelocity,
+        [Description("Find Volumetric Flow rate")]
         VolFlowRate,
+        [Description("Find Flow Rate using Dead Time")]
         VolFlowRateUsingDeadTime
     }
 }
--- MolecularWeightCalculatorLib/MoleMassDilutionTools/MoleMassDilutionEnums.cs
+++ MolecularWeightCalculatorLib/MoleMassDilutionTools/MoleMassDilutionEnums.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
 
 // ReSharper disable once CheckNamespace
 namespace MolecularWeightCalculator
@@ -6,9 +7,13 @@ namespace MolecularWeightCalculator
     [Guid("C4109F21-3649-44CF-BF77-8DC784C801B3"), ComVisible(true)]
     public enum AutoComputeDilutionMode
     {
+        [Description("Find Solution Volumes")]
         FindRequiredDilutionVolumes = 0,
+        [Description("Find Solvent and Final Volumes")]
         FindRequiredTotalVolume,
+        [Description("Find Final Concentration")]
         FindFinalConcentration,
+        [Description("Find Initial Concentration")]
         FindInitialConcentration
     }
 
@@ -23,59 +28,103 @@ namespace MolecularWeightCalculator
     [Guid("02F2CF0A-E219-48B5-8CEB-AFCACC3FBB91"), ComVisible(true)]
     public enum Unit
     {
+        [Description("Moles")]
         Moles = 0,
+        [Description("milliMoles")]
         Millimoles,
+        [Description("microMoles")]
         MicroMoles,
+        [Description("nanoMoles")]
         NanoMoles,
+        [Description("picoMoles")]
         PicoMoles,
+        [Description("femtoMoles")]
         FemtoMoles,
+        [Description("attoMoles")]
         AttoMoles,
+        [Description("Kilograms")]
         Kilograms,
+        [Description("Grams")]
         Grams,
+        [Description("Milligrams")]
         Milligrams,
+        [Description("Micrograms")]
         Micrograms,
+        [Description("Pounds")]
         Pounds,
+        [Description("Ounces")]
         Ounces,
+        [Description("Liters")]
         Liters,
+        [Description("Deciliters")]
         DeciLiters,
+        [Description("Milliliters")]
         MilliLiters,
+        [Description("Microliters")]
         MicroLiters,
+        [Description("Nanoliters")]
         NanoLiters,
+        [Description("Picoliters")]
         PicoLiters,
+        [Description("Gallons")]
         Gallons,
+        [Description("Quarts")]
         Quarts,
+        [Description("Pints")]
         Pints
     }
 
     [Guid("0FD1BC9F-19C0-4449-B639-55A20187E6E2"), ComVisible(true)]
     public enum UnitOfExtendedVolume
     {
+        [Description("Liters")]
         L = 0,
+        [Description("Deciliters")]
         DL,
+        [Description("Milliliters")]
         ML,
+        [Description("Microliters")]
         UL,
+        [Description("Nanoliters")]
         NL,
+        [Description("Picoliters")]
         PL,
+        [Description("Gallons")]
         Gallons,
+        [Description("Quarts")]
         Quarts,
+        [Description("Pints")]
         Pints
     }
 
     [Guid("5D63EAA6-FC5C-46F9-AD96-74DD8DF2BF44"), ComVisible(true)]
     public enum UnitOfMoleMassConcentration
     {
+        [Description("Molar")]
         Molar = 0,
+        [Description("milliMolar")]
         MilliMolar,
+        [Description("microMolar")]
         MicroMolar,
+        [Description("nanoMolar")]
         NanoMolar,
+        [Description("picoMolar")]
         PicoMolar,
+        [Description("femtoMolar")]
         FemtoMolar,
+        [Description("attoMolar")]
         AttoMolar,
+        [Description("mg/dL")]
         MgPerDL,
+        [Description("mg/mL")]
         MgPerML,
+        [Description("ug/mL")]
         UgPerML,
+        [Description("ng/mL")]
         NgPerML,
+        [Description("ug/uL")]
         UgPerUL,
+        [Description("ng/uL")]
         NgPerUL
     }
 }
