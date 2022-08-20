aea7721b8425385216aa2f73d02f01f42cb222dd
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2022-08-19 18:41:01 -0700

    About window: Show hyperlinks, allow click, (limited) selection, and right-click menu copy

--- MolecularWeightCalculatorGUI/AboutWindow.xaml.cs
+++ MolecularWeightCalculatorGUI/AboutWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
 using System.Windows;
+using System.Windows.Navigation;
 using MolecularWeightCalculatorGUI.Properties;
 
 namespace MolecularWeightCalculatorGUI
@@ -22,6 +25,33 @@ namespace MolecularWeightCalculatorGUI
         private void Close_OnClick(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            // .NET Framework: UseShellExecute defaults to true, so we don't need to directly set it
+            // .NET Core: UseShellExecute defaults to false, and we need ShellExecute
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+        }
+
+        private void CopyEmailLink(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(Email.NavigateUri.AbsoluteUri.Replace("mailto:", ""), TextDataFormat.Text);
+        }
+
+        private void CopyGitHubRepoLink(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(GitHubRepo.NavigateUri.AbsoluteUri, TextDataFormat.Text);
+        }
+
+        private void CopyGitHubPageLink(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(GitHubPage.NavigateUri.AbsoluteUri, TextDataFormat.Text);
+        }
+
+        private void CopyAlchemistMattPageLink(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(AlchemistMatt.NavigateUri.AbsoluteUri, TextDataFormat.Text);
         }
     }
 }
